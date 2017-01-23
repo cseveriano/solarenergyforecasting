c0001fb4c87ce8f0c42f5531b34ab446931b6035
Carlos Severiano <carlossjr@gmail.com>
2017-01-23 16:26:02 -0200

    Read directory and resample to 30 minutes

--- pyFTS/tests/pfts.py
+++ pyFTS/tests/pfts.py
@@ -2,8 +2,10 @@
 # -*- coding: utf8 -*-
 
 import os
+import glob
 import numpy as np
 import pandas as pd
+from pandas import Series, DataFrame, Panel
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D
 
@@ -17,19 +19,45 @@ from pyFTS import pfts
 #from pyFTS.benchmarks import benchmarks as bchmk
 
 os.chdir("C:\\Users\\cseve\\Google Drive\\Doutorado\\Projetos de Pesquisa\\Base de Dados\\INPE-SONDA")
+path = "C:\\Users\\cseve\\Google Drive\\Doutorado\\Projetos de Pesquisa\\Base de Dados\\INPE-SONDA"
 
-station = pd.read_csv("BRB1301ED.csv", sep=";")
-enrollments = np.array(enrollments["Enrollments"])
+tseries = pd.DataFrame()
 
-enrollments_fs1 = Grid.GridPartitionerTrimf(enrollments,6)
-
-pfts1_enrollments = pfts.ProbabilisticFTS("1")
-pfts1_enrollments.train(enrollments,enrollments_fs1,1)
-pfts1_enrollments.shortname = "1st Order"
-pfts2_enrollments = pfts.ProbabilisticFTS("2")
-pfts2_enrollments.dump = False
-pfts2_enrollments.shortname = "2nd Order"
-pfts2_enrollments.train(enrollments,enrollments_fs1,2)
+#read the header
+header = pd.read_csv("ED_header_new.csv", sep=";")
 
 
-pfts1_enrollments.forecastAheadDistribution2(enrollments[:15],5,100)
+#read the whole directory
+all_files = glob.glob(os.path.join(path, "*ED.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
+
+np_array_list = []
+for file_ in all_files:
+    df = pd.read_csv(file_, header=None, sep=";")
+    print (file_,  " " , df.shape)
+    np_array_list.append(df.as_matrix())
+
+comb_np_array = np.vstack(np_array_list)
+big_frame = pd.DataFrame(comb_np_array)
+
+big_frame.columns = header.columns
+
+#####
+
+# set time series range
+dates = pd.date_range("2013-01-01 00:00", "2015-12-01 00:00", freq="1min")
+dates = dates[:-1]
+ts = pd.Series(big_frame["glo_avg"].values, index=dates)
+ts30 = ts.resample("30min")
+
+#enrollments_fs1 = Grid.GridPartitionerTrimf(enrollments,6)
+
+#pfts1_enrollments = pfts.ProbabilisticFTS("1")
+#pfts1_enrollments.train(enrollments,enrollments_fs1,1)
+#pfts1_enrollments.shortname = "1st Order"
+#pfts2_enrollments = pfts.ProbabilisticFTS("2")
+#pfts2_enrollments.dump = False
+#pfts2_enrollments.shortname = "2nd Order"
+#pfts2_enrollments.train(enrollments,enrollments_fs1,2)
+
+
+#pfts1_enrollments.forecastAheadDistribution2(enrollments[:15],5,100)
